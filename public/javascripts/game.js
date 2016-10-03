59e868b01647fa157bdd61895d8862a77886084d
Andre Gavazzoni <andrej@gazetadopovo.com.br>
2016-10-03 16:46:16 -0300

    Better Sprites and Collision

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -3,13 +3,21 @@ window.addEventListener("load", function () {
             preload: function () {
                 game.load.image("priest", "images/padre.png");
                 game.load.image("church", "images/church.jpg");
+                game.load.image("nun", "images/freira.png")
             },
             create: function () {
+                game.physics.startSystem(Phaser.Physics.ARCADE);
+                game.physics.setBoundsToWorld();
+                nunGroup = game.add.group();
+
                 var church = game.add.sprite(0, 0, "church");
                 priest = game.add.sprite(game.width * 0.5, game.height - 120, "priest");
+                game.physics.enable(priest, Phaser.Physics.ARCADE);
+                priest.body.collideWorldBounds = true;
+                priest.scale.setTo(0.2, 0.2);
+
                 church.width = game.width;
                 church.height = game.height;
-                priest.scale.setTo(0.2, 0.2);
             },
             update: function () {
                 var keyboard = game.input.keyboard;
@@ -25,7 +33,9 @@ window.addEventListener("load", function () {
         };
 
         var speed = 3;
-        var priest;
+        var priest, nun, nunGroup;
+        var maxNunOnScreen = 4;
+        var points = 0;
         var game = new Phaser.Game(400, 600, Phaser.AUTO, 'gameDiv', states);
     }
 );
