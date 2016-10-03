4a7e974460975f236d88cb74d2295b16e603fff4
Andre Gavazzoni <andrej@gazetadopovo.com.br>
2016-10-03 17:07:34 -0300

    Trying to making rain nun

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -8,19 +8,30 @@ window.addEventListener("load", function () {
             create: function () {
                 game.physics.startSystem(Phaser.Physics.ARCADE);
                 game.physics.setBoundsToWorld();
-                nunGroup = game.add.group();
+                game.physics.arcade.gravity.y = -100;
+
+                nunGroup = game.add.physicsGroup();
+
 
                 var church = game.add.sprite(0, 0, "church");
                 priest = game.add.sprite(game.width * 0.5, game.height - 120, "priest");
                 game.physics.enable(priest, Phaser.Physics.ARCADE);
                 priest.body.collideWorldBounds = true;
                 priest.scale.setTo(0.2, 0.2);
+                priest.body.allowGravity = false;
 
                 church.width = game.width;
                 church.height = game.height;
+
+                this.timer = game.time.events.loop(1500, this.addNun, this);
+
             },
             update: function () {
                 var keyboard = game.input.keyboard;
+
+                for (var i = 0, len = nunGroup.children.length; i < len; i++) {
+                    nunGroup.children[i].y += 10;
+                }
 
                 if (keyboard.isDown(Phaser.Keyboard.LEFT)) {
                     priest.x -= speed;
@@ -29,12 +40,24 @@ window.addEventListener("load", function () {
                 if (keyboard.isDown(Phaser.Keyboard.RIGHT)) {
                     priest.x += speed;
                 }
+            },
+            addNun: function() {
+                var x = game.rnd.between(40, 340);
+                var nun = game.add.sprite(x, 100, 'nun');
+                nunGroup.add(nun);
+                game.physics.arcade.enable(nun);
+                nun.checkWorldBounds = true;
+                nun.outOfBoundsKill = true;
+                nun.body.velocity.y += 10;
+
+                console.log(nun);
             }
         };
 
         var speed = 3;
         var priest, nun, nunGroup;
-        var maxNunOnScreen = 4;
+        var MAX_NUN = 6;
+        var nunCount = 0;
         var points = 0;
         var game = new Phaser.Game(400, 600, Phaser.AUTO, 'gameDiv', states);
     }
