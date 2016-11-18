e7ec6036c579fe93b60d31cbe0f5b390958592d3
Andre Gavazzoni <andrej@gazetadopovo.com.br>
2016-11-18 16:34:34 -0200

    Refactoring just for the lulz

---
+++ web-src/javascripts/game/RainingNun.js
@@ -0,0 +1,11 @@
+var RainingNun = RainingNun || {};
+
+RainingNun.game = new Phaser.Game(320, 480, Phaser.AUTO, "game-container");
+
+RainingNun.game.state.add('Boot', RainingNun.Boot);
+RainingNun.game.state.add("Preload", RainingNun.Preload);
+RainingNun.game.state.add("MainMenu", RainingNun.MainMenu);
+RainingNun.game.state.add("Game", RainingNun.Game);
+RainingNun.game.state.add("Highscore", RainingNun.Highscore);
+RainingNun.game.state.start("Boot");
+
